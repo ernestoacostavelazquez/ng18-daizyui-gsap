7b8484ea96657f2cd3a3e116c9416ddda5ee93d2
ernestoacostavelazquez <ernesto.acostavelazquez@gmail.com>
2024-06-25 11:40:34 -0600

    actulacion

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-progress',
@@ -8,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrl: './progress.component.css'
 })
 export class ProgressComponent {
+
+  ngOnInit():void{
+    this.animatedProgressBars();
+  }
+
+  animatedProgressBars(){
+    gsap.to('#htmlProgress',{
+      value:90,
+      duration:2,
+      ease:"poer2.inOut"
+    });
+    gsap.to('#cssProgress',{
+      value:80,
+      duration:2,
+      ease:"poer2.inOut",
+      delay:0.5
+    });
+    gsap.to('#jsProgress',{
+      value:70,
+      duration:2,
+      ease:"poer2.inOut",
+      delay:1
+    });
+    gsap.to('#angularProgress',{
+      value:80,
+      duration:2,
+      ease:"poer2.inOut",
+      delay:1.5
+    });
+    gsap.to('#reactProgress',{
+      value:80,
+      duration:2,
+      ease:"poer2.inOut",
+      delay:2
+    });
+    gsap.to('#phpProgress',{
+      value:90,
+      duration:2,
+      ease:"poer2.inOut",
+      delay:2.5
+    });
+
+  }
 
 }
